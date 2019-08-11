42f713a5e8d9849886291ee8f4b1f9c70f8fe719
Kelson Warner <kelson@null.net>
2019-08-11 11:07:21 -0700

    adding pusher config

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const Pusher = require('pusher');
 
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/api', (req, res) => {
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  useTLS: true,
+});
+
+app.post('/pusher/auth', (req, res) => {
+  const socketId = req.body.socket_id;
+  const channel = req.body.channel_name;
+  const auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+app.get('/api', (_, res) => {
   const info = {
     success: true,
   };
@@ -10,6 +31,6 @@ app.get('/api', (req, res) => {
   res.json(info);
 });
 
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => `Server running on port ${port}`);
